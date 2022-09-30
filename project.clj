6da3af0b824081a84c67bfb02a463191a18b0a1e
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-09-30 12:59:04 +0900

    今度は js エラーにならない。

--- project.clj
+++ project.clj
@@ -12,7 +12,8 @@
                  [clojure.java-time "1.1.0"]
                  [com.cognitect/transit-clj "1.0.329"]
                  [com.cognitect/transit-cljs "0.8.280"]
-                 [com.google.javascript/closure-compiler-unshaded "v20220803"]
+                 ;; [com.google.javascript/closure-compiler-unshaded "v20220803"]
+                 [com.google.javascript/closure-compiler-unshaded "v20220905"]
                  [conman "0.9.5"]
                  [cprop "0.1.19"]
                  [expound "0.9.0"]
@@ -64,13 +65,13 @@
              :prep-tasks ["compile" ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]]
              :aot :all
              :uberjar-name "wil.jar"
-             :source-paths ["env/prod/clj"  "env/prod/cljs" ]
+             :source-paths ["env/prod/clj"  "env/prod/cljs"]
              :resource-paths ["env/prod/resources"]}
 
    :dev           [:project/dev :profiles/dev]
    :test          [:project/dev :project/test :profiles/test]
 
-   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn" ]
+   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn"]
                   :dependencies [[binaryage/devtools "1.0.6"]
                                  [cider/piggieback "0.5.3"]
                                  [org.clojure/tools.namespace "1.3.0"]
@@ -83,16 +84,16 @@
                                  [cider/cider-nrepl "0.28.6"]] 
                   
                   
-                  :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs" ]
+                  :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs"]
                   :resource-paths ["env/dev/resources"]
                   :repl-options {:init-ns user
                                  :timeout 120000}
                   :injections [(require 'pjstadig.humane-test-output)
                                (pjstadig.humane-test-output/activate!)]}
-   :project/test {:jvm-opts ["-Dconf=test-config.edn" ]
-                  :resource-paths ["env/test/resources"] 
+   :project/test {:jvm-opts ["-Dconf=test-config.edn"]
+                  :resource-paths ["env/test/resources"]} 
                   
                   
-                  }
+                  
    :profiles/dev {}
    :profiles/test {}})
