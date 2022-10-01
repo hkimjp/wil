6b9019186dd73a9026900d41eef1eea8356d05cb
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-10-01 14:37:38 +0900

    under construction

--- resources/migrations/20220930050818-notes.up.sql
+++ resources/migrations/20220930050818-notes.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE notes (
   id         SERIAL PRIMARY KEY,
   login      VARCHAR(20),
-  date       DATE,
+  date       CHAR(10),
   note       TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
